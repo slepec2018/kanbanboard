dca989a48eb9165b8f0cbb0c840c7e9803feb39f
slepec <slepec2018@gmail.com>
2023-07-17 19:48:08 +0100

    kanbanboard

---
+++ js/data.js
@@ -0,0 +1,47 @@
+export const tasks = [
+  {
+    'id': `1`,
+    'title': `Learn JS`,
+    'status': `backlog`,
+  },
+  {
+    'id': `2`,
+    'title': `Learn React`,
+    'status': `backlog`,
+  },
+  {
+    'id': `3`,
+    'title': `Call Mom`,
+    'status': `done`,
+  },
+  {
+    'id': `4`,
+    'title': `Drink a smoothie`,
+    'status': `processing`,
+  },
+  {
+    'id': `5`,
+    'title': `Go for a walk`,
+    'status': `basket`,
+  },
+  {
+    'id': `6`,
+    'title': `Stop being dumb`,
+    'status': `processing`,
+  },
+  {
+    'id': `7`,
+    'title': `Do homework`,
+    'status': `backlog`,
+  },
+  {
+    'id': `8`,
+    'title': `Read War and Peace`,
+    'status': `basket`,
+  },
+  {
+    'id': `9`,
+    'title': `Pet the cat`,
+    'status': `done`,
+  },
+];
